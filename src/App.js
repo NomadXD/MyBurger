5f383e219e6abb49f9860e0f506d5dc043217528
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-02-14 11:30:51 +0530

    component tree created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Layout from '../src/components/Layout/Layout'
+import BurgerBuilder from '../src/containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <p>This is a test</p>
+        <BurgerBuilder></BurgerBuilder>
       </Layout>
     );
   }
