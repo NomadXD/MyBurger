b9b48aaf0099bf70526e737bb74ad53026ec0cb5
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-02-14 12:00:14 +0530

    state added for BurgerBuilder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,18 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
+
+    const ingredientsArray = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_,i)=>{
+                return <BurgerIngredient key={igKey+i} type={igKey}></BurgerIngredient>
+            })
+        })
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {ingredientsArray}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
