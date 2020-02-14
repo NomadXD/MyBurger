7d672cd5870ac04a51a0275e341e6d9ccb1fbcce
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-02-14 10:17:50 +0530

    file structure created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from '../src/components/Layout/Layout'
+
+class App extends Component {
+  render() {
+    return (
+      <Layout>
+        <p>This is a test</p>
+      </Layout>
+    );
+  }
+}
+
+export default App;
