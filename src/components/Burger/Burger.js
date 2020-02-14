886310b540a9208381ae156945f3456c35779329
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2020-02-14 14:36:54 +0530

    build controls added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
 
-    const ingredientsArray = Object.keys(props.ingredients)
+    let ingredientsArray = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_,i)=>{
                 return <BurgerIngredient key={igKey+i} type={igKey}></BurgerIngredient>
             })
         })
+        .reduce((arr,el)=>{
+            return arr.concat(el)
+        },[])
+    if(ingredientsArray.length === 0){
+        ingredientsArray = <p>Please add ingredients</p>
+    }
 
     return(
         <div className={classes.Burger}>
